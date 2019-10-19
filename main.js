a8dc4b3701323cafeb2ca4a3c7d87100969e09c5
alex <alex110745@hotmail.fr>
2019-10-19 18:27:56 +0200

    ajout d'option

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const { Client } = require('discord.js');
 const client = new Client({disableEveryone: true});
+const { TOKEN, PREFIX} = require('./config');
 
 
 //const Discord = require('discord.js');
 //const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('Bot ready');
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') msg.reply('Pong!');
-  if (msg.content === 'everyone') msg.reply('@everyone, salut à tous!', {disableEveryone: false});
-  if (msg.content === 'noteveryone') msg.reply('@everyone, salut à tous');
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.reply('Pong!');
 });
 
-client.login('NjMzMzM3MzMwNzQ0NDI2NTE2.XaS3Gw.b9R619ATf48hT4jc4iXL7m3JP5E');
+client.login(TOKEN);
